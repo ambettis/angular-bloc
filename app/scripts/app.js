7dc4047d325ca5321c73ecf36438dfcd6b033caf
Austin Bettis <austinbettis@Austins-MacBook-Pro.local>
2017-08-08 22:13:51 -0500

    assignment angular-3 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,11 @@ angular.module('blocJams', ['ui.router']);
              url: '/album',
              templateUrl: '/templates/album.html'
          });
+
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
+         });
   }
 
   angular.module('blocJams', ['ui.router']);
