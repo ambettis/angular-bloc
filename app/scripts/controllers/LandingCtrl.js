c6bd3f72e6870143229341b162fa7f4e8e5edcd5
Austin Bettis <austinbettis@Austins-MacBook-Pro.local>
2017-08-10 21:23:31 -0500

    minor fixes

---
+++ app/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,16 @@
+// (function() {
+//      function LandingCtrl() {
+//        this.headingText = 'BOOM!'
+//        this.alertMe = function(){
+//          alert('you rang?!?')
+//        }
+//        this.age = 400
+//        this.alertMeWithValue = function(val){
+//          alert(val)
+//        }
+//      }
+//
+//      angular
+//          .module('blocJams')
+//          .controller('LandingCtrl', LandingCtrl);
+//  })();
