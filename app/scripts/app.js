0409cfbf9b71e3f6d358d278c6503e9889b367eb
Austin Bettis <austinbettis@Austins-MacBook-Pro.local>
2017-08-14 21:57:50 -0500

    Controlers assignment. Help needed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,3 +32,18 @@ angular.module('blocJams', ['ui.router']);
     .module('blocJams', ['ui.router'])
     .config(config);
 })();
+
+this.albumData = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/bloc_jams_music/blue' },
+        { title: 'Green', duration: 103.96, audioUrl: 'assets/music/bloc_jams_music/green' },
+        { title: 'Red', duration: 268.45, audioUrl: 'assets/music/bloc_jams_music/red' },
+        { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/bloc_jams_music/pink' },
+        { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/bloc_jams_music/magenta' }
+    ]
+};
--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -7,20 +7,3 @@
         .module('blocJams')
         .controller('LandingCtrl', LandingCtrl);
 })();
-
-// (function() {
-//      function LandingCtrl() {
-//        this.headingText = 'BOOM!'
-//        this.alertMe = function(){
-//          alert('you rang?!?')
-//        }
-//        this.age = 400
-//        this.alertMeWithValue = function(val){
-//          alert(val)
-//        }
-//      }
-//
-//      angular
-//          .module('blocJams')
-//          .controller('LandingCtrl', LandingCtrl);
-//  })();
